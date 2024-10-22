9d481a101ed83d66e43173fe2ba85a1cfc295eb4
Andrea Montemurro <andrea.montemurro97@gmail.com>
2024-10-22 15:06:37 +0200

    using mixed precision trainer.

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.amp import autocast, GradScaler
 from tqdm import tqdm
 
 from .utils import pixel_accuracy, mIoU, plot_loss, plot_score, plot_acc
@@ -20,10 +21,10 @@ class Trainer:
             lr (float): Learning rate for the optimizer.
             weight_decay (float): Weight decay for the optimizer.
         """
+        self.device = device
         self.model = model.to(device)
         self.dataloader_train = dataloader_train
         self.dataloader_val = dataloader_val
-        self.device = device
         self.num_classes = num_classes
         
         # Loss function: Cross-Entropy Loss for multi-class segmentation
@@ -31,66 +32,50 @@ class Trainer:
         
         # Optimizer: AdamW optimizer
         self.optimizer = optim.AdamW(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+
+        # Initialize the gradient scaler
+        self.scaler = GradScaler()  
+    
     
     def train_one_epoch(self, epoch):
-        """
-        Train the model for one epoch.
-        
-        Args:
-            epoch (int): Current epoch number.
-        
-        Returns:
-            float: Training loss.
-            float: Training pixel accuracy.
-            float: Training mean IoU.
-        """
         self.model.train()
         running_loss = 0.0
         running_acc = 0.0
         running_miou = 0.0
 
         for batch in tqdm(self.dataloader_train, desc=f"Epoch {epoch+1} [Train]"):
-            # Get inputs and labels
             inputs, labels = batch
             inputs = inputs.to(self.device)
             labels = labels.to(self.device)
 
-            # Zero the gradients
             self.optimizer.zero_grad()
 
-            # Forward pass
-            outputs = self.model(inputs)
+            # autocast context reduces the precision for certain operations to speed up training and reduce memory usage
+            with autocast(device_type=self.device.type):
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
 
-            # Compute loss
-            loss = self.criterion(outputs, labels)
+            # Scales the loss, and calls backward() to create scaled gradients
+            self.scaler.scale(loss).backward()
+            
+            # Scaler step updates the weights and unscale the gradients
+            self.scaler.step(self.optimizer)
+            
+            # Updates the scale for the next iteration
+            self.scaler.update()
 
-            # Backward pass and optimization
-            loss.backward()
-            self.optimizer.step()
-
-            # Compute metrics
             running_loss += loss.item()
             running_acc += pixel_accuracy(outputs, labels)
             running_miou += mIoU(outputs, labels, n_classes=self.num_classes)
-        
+
         avg_loss = running_loss / len(self.dataloader_train)
         avg_acc = running_acc / len(self.dataloader_train)
         avg_miou = running_miou / len(self.dataloader_train)
 
         return avg_loss, avg_acc, avg_miou
 
+
     def validate(self, epoch):
-        """
-        Validate the model on the validation dataset.
-        
-        Args:
-            epoch (int): Current epoch number.
-        
-        Returns:
-            float: Validation loss.
-            float: Validation pixel accuracy.
-            float: Validation mean IoU.
-        """
         self.model.eval()
         running_loss = 0.0
         running_acc = 0.0
@@ -98,27 +83,24 @@ class Trainer:
 
         with torch.no_grad():
             for batch in tqdm(self.dataloader_val, desc=f"Epoch {epoch+1} [Validate]"):
-                # Get inputs and labels
                 inputs, labels = batch
                 inputs = inputs.to(self.device)
                 labels = labels.to(self.device)
 
-                # Forward pass
-                outputs = self.model(inputs)
+                with autocast():
+                    outputs = self.model(inputs)
+                    loss = self.criterion(outputs, labels)
 
-                # Compute loss
-                loss = self.criterion(outputs, labels)
-
-                # Compute metrics
                 running_loss += loss.item()
                 running_acc += pixel_accuracy(outputs, labels)
                 running_miou += mIoU(outputs, labels, n_classes=self.num_classes)
-        
+
         avg_loss = running_loss / len(self.dataloader_val)
         avg_acc = running_acc / len(self.dataloader_val)
         avg_miou = running_miou / len(self.dataloader_val)
 
         return avg_loss, avg_acc, avg_miou
+
 
     def train(self, num_epochs):
         """
